76fd61af7d9399b5f7bb901f4f3c963391577762
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2020-07-22 09:09:49 -0500

    v0.0.7
    • Conditioanl formation for completed task

--- src/components/ToDoList/TodoItem.js
+++ src/components/ToDoList/TodoItem.js
@@ -12,13 +12,14 @@ class ToDoItem extends Component {
     }
 
     render() {
+        let displayItemText = this.props.listItem.completed && "todolist__text--completed"
         return (
             <div className="todolist__item">
                 <label className="todolist__customcheckbox">
                     <input className="todolist__checkbox" type="checkbox" checked={this.props.listItem.completed} onChange={this.handleClick}/>
                     <span className="todolist__tick"></span>
                 </label>
-                <p className="todolist__text">{this.props.listItem.text}</p>
+                <p className={"todolist__text " + displayItemText}>{this.props.listItem.text}</p>
             </div>
         )
     }
